80381f8d7a076784fbc7f5f2bf64cab86ef8cd31
Christian <Christian@DESKTOP-NBDTTBA>
2022-04-22 10:30:37 +0700

    Add project files.

---
+++ Materi22April/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Materi22April
+{
+    partial class FormLatihan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboHome = new System.Windows.Forms.ComboBox();
+            this.comboAway = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labStadium = new System.Windows.Forms.Label();
+            this.labCapacity = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.labHomeManager = new System.Windows.Forms.Label();
+            this.labHomeAssManager = new System.Windows.Forms.Label();
+            this.labHomeKapten = new System.Windows.Forms.Label();
+            this.labAwayKapten = new System.Windows.Forms.Label();
+            this.labAwayAssManager = new System.Windows.Forms.Label();
+            this.labAwayManager = new System.Windows.Forms.Label();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboHome
+            // 
+            this.comboHome.FormattingEnabled = true;
+            this.comboHome.Location = new System.Drawing.Point(12, 35);
+            this.comboHome.Name = "comboHome";
+            this.comboHome.Size = new System.Drawing.Size(121, 21);
+            this.comboHome.TabIndex = 0;
+            this.comboHome.SelectedIndexChanged += new System.EventHandler(this.comboHome_SelectedIndexChanged);
+            // 
+            // comboAway
+            // 
+            this.comboAway.FormattingEnabled = true;
+            this.comboAway.Location = new System.Drawing.Point(441, 35);
+            this.comboAway.Name = "comboAway";
+            this.comboAway.Size = new System.Drawing.Size(121, 21);
+            this.comboAway.TabIndex = 1;
+            this.comboAway.SelectedIndexChanged += new System.EventHandler(this.comboAway_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Team Home";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(471, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Team Away";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "VS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Stadium : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(242, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Capacity : ";
+            // 
+            // labStadium
+            // 
+            this.labStadium.AutoSize = true;
+            this.labStadium.Location = new System.Drawing.Point(303, 181);
+            this.labStadium.Name = "labStadium";
+            this.labStadium.Size = new System.Drawing.Size(16, 13);
+            this.labStadium.TabIndex = 7;
+            this.labStadium.Text = "---";
+            // 
+            // labCapacity
+            // 
+            this.labCapacity.AutoSize = true;
+            this.labCapacity.Location = new System.Drawing.Point(303, 208);
+            this.labCapacity.Name = "labCapacity";
+            this.labCapacity.Size = new System.Drawing.Size(16, 13);
+            this.labCapacity.TabIndex = 8;
+            this.labCapacity.Text = "---";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Nama Manager : ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 96);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(126, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Nama Asisten Manager : ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 122);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 13);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Nama Kapten : ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(390, 122);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Nama Kapten : ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(390, 96);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(126, 13);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Nama Asisten Manager : ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(390, 71);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(89, 13);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Nama Manager : ";
+            // 
+            // labHomeManager
+            // 
+            this.labHomeManager.AutoSize = true;
+            this.labHomeManager.Location = new System.Drawing.Point(107, 71);
+            this.labHomeManager.Name = "labHomeManager";
+            this.labHomeManager.Size = new System.Drawing.Size(16, 13);
+            this.labHomeManager.TabIndex = 15;
+            this.labHomeManager.Text = "---";
+            // 
+            // labHomeAssManager
+            // 
+            this.labHomeAssManager.AutoSize = true;
+            this.labHomeAssManager.Location = new System.Drawing.Point(144, 96);
+            this.labHomeAssManager.Name = "labHomeAssManager";
+            this.labHomeAssManager.Size = new System.Drawing.Size(16, 13);
+            this.labHomeAssManager.TabIndex = 16;
+            this.labHomeAssManager.Text = "---";
+            // 
+            // labHomeKapten
+            // 
+            this.labHomeKapten.AutoSize = true;
+            this.labHomeKapten.Location = new System.Drawing.Point(99, 122);
+            this.labHomeKapten.Name = "labHomeKapten";
+            this.labHomeKapten.Size = new System.Drawing.Size(16, 13);
+            this.labHomeKapten.TabIndex = 17;
+            this.labHomeKapten.Text = "---";
+            // 
+            // labAwayKapten
+            // 
+            this.labAwayKapten.AutoSize = true;
+            this.labAwayKapten.Location = new System.Drawing.Point(477, 122);
+            this.labAwayKapten.Name = "labAwayKapten";
+            this.labAwayKapten.Size = new System.Drawing.Size(16, 13);
+            this.labAwayKapten.TabIndex = 20;
+            this.labAwayKapten.Text = "---";
+            // 
+            // labAwayAssManager
+            // 
+            this.labAwayAssManager.AutoSize = true;
+            this.labAwayAssManager.Location = new System.Drawing.Point(522, 96);
+            this.labAwayAssManager.Name = "labAwayAssManager";
+            this.labAwayAssManager.Size = new System.Drawing.Size(16, 13);
+            this.labAwayAssManager.TabIndex = 19;
+            this.labAwayAssManager.Text = "---";
+            // 
+            // labAwayManager
+            // 
+            this.labAwayManager.AutoSize = true;
+            this.labAwayManager.Location = new System.Drawing.Point(485, 71);
+            this.labAwayManager.Name = "labAwayManager";
+            this.labAwayManager.Size = new System.Drawing.Size(16, 13);
+            this.labAwayManager.TabIndex = 18;
+            this.labAwayManager.Text = "---";
+            // 
+            // labelValue
+            // 
+            this.labelValue.AutoSize = true;
+            this.labelValue.Location = new System.Drawing.Point(144, 38);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(16, 13);
+            this.labelValue.TabIndex = 21;
+            this.labelValue.Text = "---";
+            // 
+            // FormLatihan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 516);
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.labAwayKapten);
+            this.Controls.Add(this.labAwayAssManager);
+            this.Controls.Add(this.labAwayManager);
+            this.Controls.Add(this.labHomeKapten);
+            this.Controls.Add(this.labHomeAssManager);
+            this.Controls.Add(this.labHomeManager);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.labCapacity);
+            this.Controls.Add(this.labStadium);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboAway);
+            this.Controls.Add(this.comboHome);
+            this.Name = "FormLatihan";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormLatihan_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboHome;
+        private System.Windows.Forms.ComboBox comboAway;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labStadium;
+        private System.Windows.Forms.Label labCapacity;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label labHomeManager;
+        private System.Windows.Forms.Label labHomeAssManager;
+        private System.Windows.Forms.Label labHomeKapten;
+        private System.Windows.Forms.Label labAwayKapten;
+        private System.Windows.Forms.Label labAwayAssManager;
+        private System.Windows.Forms.Label labAwayManager;
+        private System.Windows.Forms.Label labelValue;
+    }
+}
+
---
+++ Materi22April/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Materi22April
+{
+    public partial class FormLatihan : Form
+    {
+        public FormLatihan()
+        {
+            InitializeComponent();
+        }
+
+        static string connectionString = "server=localhost;uid=root;pwd=;database=premier_league;";
+        public MySqlConnection sqlConnect = new MySqlConnection(connectionString);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter;
+        public string sqlQuery;
+
+        DataTable dtTeamAway = new DataTable();
+        DataTable dtTeamHome = new DataTable();
+
+        private void FormLatihan_Load(object sender, EventArgs e)
+        {
+            sqlQuery = "SELECT t.team_id as `ID Tim`, t.team_name as `Nama Tim`, m.manager_name as `Nama Manager`, IF(m2.manager_name IS NULL,'----',m2.manager_name) as `Nama Asisten Manager`,p.player_name as `Nama Kapten`, home_stadium as `Stadium`, capacity as `Kapasitas` FROM team t LEFT JOIN manager m2 ON  t.assmanager_id = m2.manager_id ,manager m, player p WHERE t.manager_id = m.manager_id AND t.captain_id = p.player_id ;";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtTeamHome);
+            sqlAdapter.Fill(dtTeamAway);
+            comboHome.DataSource = dtTeamHome;
+            comboHome.DisplayMember = "Nama Tim";
+            comboHome.ValueMember = "ID Tim";
+            comboAway.DataSource = dtTeamAway;
+            comboAway.DisplayMember = "Nama Tim";
+        }
+
+        private void comboHome_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int posisiIndex = comboHome.SelectedIndex;
+            labHomeManager.Text = dtTeamHome.Rows[posisiIndex]["Nama Manager"].ToString();
+            labHomeAssManager.Text = dtTeamHome.Rows[posisiIndex]["Nama Asisten Manager"].ToString();
+            labHomeKapten.Text = dtTeamHome.Rows[posisiIndex]["Nama Kapten"].ToString();
+            labStadium.Text = dtTeamHome.Rows[posisiIndex]["Stadium"].ToString();
+            labCapacity.Text = dtTeamHome.Rows[posisiIndex]["Kapasitas"].ToString();
+        }
+
+        private void comboAway_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int posisiIndex = comboAway.SelectedIndex;
+            labAwayManager.Text = dtTeamAway.Rows[posisiIndex]["Nama Manager"].ToString();
+            labAwayAssManager.Text = dtTeamAway.Rows[posisiIndex]["Nama Asisten Manager"].ToString();
+            labAwayKapten.Text = dtTeamAway.Rows[posisiIndex]["Nama Kapten"].ToString();
+        }
+    }
+}
